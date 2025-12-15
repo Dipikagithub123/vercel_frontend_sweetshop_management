eeced7cc07beb3af34405ccd445f7c88e0d0923b
Dipikagithub123 <155236376+Dipikagithub123@users.noreply.github.com>
2025-12-15 11:28:37 +0530

    Change API proxy target to production URL

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://web-production-80c12.up.railway.app/',
         changeOrigin: true,
       },
     },
